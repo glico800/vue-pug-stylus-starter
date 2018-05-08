a694cd9de54d6cfd54abdad8696eaa865b29227d
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-08 15:16:21 +0900

    convert css to stylus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -48,20 +48,17 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
 h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+li
+  display inline-block
+  margin 0 10px
+a
+  color #42b983
 </style>
